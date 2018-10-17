e8012ffc40d150e174d55832a6aaeff376fe1abe
Artemije <artem.kuznecov.samara@gmail.com>
2018-10-17 09:57:09 +0400

    extract($array);

---
+++ mvc/config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artem
+ * Date: 17.10.18
+ * Time: 9:05
+ */
+const BASE = __DIR__;
---
+++ mvc/src/MVCExample/View.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artem
+ * Date: 17.10.18
+ * Time: 9:37
+ */
+
+namespace src\MVCExample\View;
+
+
+class View
+{
+
+}
